351973822ac477a9f96c01b8a3ac9c77db1282a9
Felix <dominiquejordan813@yahoo.com>
2026-09-09 11:13:45 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Felix: Dominique Felix Jordan
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hellow World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+
+Console.WriteLine("Hello, World!");
